d645304fcc03dcd664238da3f603c114288095f7
sweetwaterrain <49685191+sweetwaterrain@users.noreply.github.com>
2023-05-14 17:05:36 +0800

    添加netwoks位置

--- pytracking/evaluation/local.py
+++ pytracking/evaluation/local.py
@@ -12,7 +12,7 @@ def local_env_settings():
     settings.got_reports_path = ''
     settings.lasot_path = ''
     settings.network_path = ''
-    settings.nfs_path = ''
+    settings.nfs_path = '/kaggle/working/trdimp/pytracking/networks'
     settings.otb_path = '/kaggle/input/otb2015/OBT100'
     settings.result_plot_path = ''
     settings.results_path = ''
@@ -21,7 +21,7 @@ def local_env_settings():
     settings.tpl_path = ''
     settings.trackingnet_path = ''
     settings.uav_path = ''
-    settings.vot_path = '/kaggle/input/vot2013/'
+    settings.vot_path = '/kaggle/input/vot2013'
     settings.youtubevos_dir = ''
 
     return settings
