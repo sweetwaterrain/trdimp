8536e4539a4ba4dcddeb169914e4ccf443021776
sweetwaterrain <49685191+sweetwaterrain@users.noreply.github.com>
2023-05-14 19:42:29 +0800

    添加忽略警告代码

--- ltr/run_training.py
+++ ltr/run_training.py
@@ -11,6 +11,9 @@ if env_path not in sys.path:
     sys.path.append(env_path)
 
 import ltr.admin.settings as ws_settings
+import warnings
+
+warnings.filterwarnings("ignore")
 
 
 def run_training(train_module, train_name, cudnn_benchmark=True):
@@ -43,7 +46,8 @@ def main():
     parser = argparse.ArgumentParser(description='Run a train scripts in train_settings.')
     parser.add_argument('train_module', type=str, help='Name of module in the "train_settings/" folder.')
     parser.add_argument('train_name', type=str, help='Name of the train settings file.')
-    parser.add_argument('--cudnn_benchmark', type=bool, default=True, help='Set cudnn benchmark on (1) or off (0) (default is on).')
+    parser.add_argument('--cudnn_benchmark', type=bool, default=True,
+                        help='Set cudnn benchmark on (1) or off (0) (default is on).')
 
     args = parser.parse_args()
 
